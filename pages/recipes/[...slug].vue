6446944fc6bdec0c66b6b96260b356b3e59cdbbc
Bruno Gonzales <holachepi@gmail.com>
2026-06-25 10:37:49 -0500

    fix empty yaml metadata rendering

--- pages/recipes/[...slug].vue
+++ pages/recipes/[...slug].vue
@@ -1,15 +1,28 @@
 <template>
   <main>
-    <ContentDoc v-slot="{ doc }">
-       <p style="margin-top: 40px;">
-        <NuxtLink to="/">&larr; Volver al índice</NuxtLink>
-      </p>
-      <h1>{{ doc.title }}</h1>
-      <ul>
-        <li v-for="(step, index) in doc.steps" :key="index">
-          {{ step }}
-        </li>
-      </ul>
+    <ContentDoc>
+      <template v-slot="{ doc }">
+        <p style="margin-top: 40px;">
+          <NuxtLink to="/">&larr; Volver al índice</NuxtLink>
+        </p>
+        <h1>{{ doc.title }}</h1>
+        <ul>
+          <li v-for="(step, index) in doc.steps" :key="index">
+            {{ step }}
+          </li>
+        </ul>
+      </template>
+      <template #empty="{ doc }">
+        <p style="margin-top: 40px;">
+          <NuxtLink to="/">&larr; Volver al índice</NuxtLink>
+        </p>
+        <h1>{{ doc.title }}</h1>
+        <ul>
+          <li v-for="(step, index) in doc.steps" :key="index">
+            {{ step }}
+          </li>
+        </ul>
+      </template>
     </ContentDoc>
   </main>
 </template>
